690e1560237e24dfd93f69b7a3cb843ddf2b3e88
Sergey Guryev <serg.gurjev@gmail.com>
2016-02-27 19:16:39 +0800

    Add the ability to create users through the admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "projects#index"
+
   namespace :admin do
     root "application#index"
 
     resources :projects, only: [:new, :create, :destroy]
+    resources :users
   end
-
-  devise_for :users
-  root "projects#index"
 
   resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
