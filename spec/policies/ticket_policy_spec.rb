8ec44a48542aec17bc4069dc7855eed1fa2f9c2b
Sergey Guryev <serg.gurjev@gmail.com>
2016-03-08 14:56:26 +0800

    Add permission for editing tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -12,28 +12,39 @@ describe TicketPolicy do
       let(:user) { nil }
 
       it { should_not permit_action :show }
-      it { should_not permit_action :create}
+      it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for viewers of the project" do
       before { assign_role!(user, :viewer, project) }
 
       it { should     permit_action :show }
-      it { should_not permit_action :create}
+      it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for editors of the project" do
       before { assign_role!(user, :editor, project) }
 
-      it { should permit_action :show }
-      it { should permit_action :create }
+      it { should     permit_action :show }
+      it { should     permit_action :create }
+      it { should_not permit_action :update }
+
+      context "when the editor created the project" do
+        before { ticket.author = user }
+
+        it { should permit_action :update }
+      end
     end
+
 
     context "for managers of the project" do
       before { assign_role!(user, :manager, project) }
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should permit_action :update }
     end
 
     context "for managers of others projects" do
@@ -43,6 +54,7 @@ describe TicketPolicy do
 
       it { should_not permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update}
     end
 
     context "for administrations" do
@@ -50,6 +62,7 @@ describe TicketPolicy do
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should permit_action :update }
     end
   end
 end
