b8876199fc7fb50d0bfbb99f93e96d16f01ca6f7
Sergey Gurjev <serg.gurjev@gmail.com>
2016-03-17 15:26:42 +0800

    Tickets can have a state assigned when comments are created

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
@@ -39,4 +40,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_controller.action_on_unpermitted_parameters = :raise
 end
