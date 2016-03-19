5d0a660334f59b2feaf80b91a7b0d124ba305a53
Sergey Gurjev <serg.gurjev@gmail.com>
2016-03-20 01:23:15 +0800

    Auto-assign the default state to newly-created tickets

--- app/models/state.rb
+++ app/models/state.rb
@@ -3,8 +3,14 @@ class State < ActiveRecord::Base
     State.update_all(default: false)
     update!(default: true)
   end
-  
+
   def to_s
     name
+  end
+
+  class << State
+    def default
+      find_by(default: true)
+    end
   end
 end
